5b170ad7c08273dc5a4d4c77e6288d771c8eb080
Bas Visscher <bas.visscher24@gmail.com>
2020-12-22 13:32:22 +0100

    before reoganizing

---
+++ ProfitManager/BinanceSync/Data.cs
@@ -0,0 +1,16 @@
+﻿using Binance.Net.Objects.Spot.UserStream;
+using STDLib.Saveable;
+using System.Collections.Generic;
+
+namespace BinanceSync
+{
+    public class Data : BaseSettingsV2<Data>
+    {
+        public List<BinanceStreamAccountInfo> BinanceStreamAccountInfos { get => GetPar(new List<BinanceStreamAccountInfo>()); }
+        public List<BinanceStreamOrderUpdate> BinanceStreamOrderUpdates { get => GetPar(new List<BinanceStreamOrderUpdate>()); }
+        public List<BinanceStreamOrderList> BinanceStreamOrderLists { get => GetPar(new List<BinanceStreamOrderList>()); }
+        public List<BinanceStreamPositionsUpdate> BinanceStreamPositionsUpdates { get => GetPar(new List<BinanceStreamPositionsUpdate>()); }
+        public List<BinanceStreamBalanceUpdate> BinanceStreamBalanceUpdates { get => GetPar(new List<BinanceStreamBalanceUpdate>()); }
+    }
+
+}
--- ProfitManager/BinanceSync/Settings.cs
+++ ProfitManager/BinanceSync/Settings.cs
@@ -7,5 +7,8 @@ namespace BinanceSync
         public string BinanceAPIKEY { get => GetPar("APIKEY"); set => SetPar(value); }
         public string BinanceAPISECRET { get => GetPar("APISECRET"); set => SetPar(value); }
 
+
+
     }
+
 }
